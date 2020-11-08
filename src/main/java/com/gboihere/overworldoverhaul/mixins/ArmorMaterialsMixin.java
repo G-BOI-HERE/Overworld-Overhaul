5a2be77b877b9a3662e639d6c5c591ebda1286ba
G-BOI-HERE <G-BOI-HERE@users.noreply.github.com>
2020-11-08 17:49:16 -0500

    update again

--- src/main/java/com/gboihere/overworldoverhaul/mixins/ArmorMaterialsMixin.java
+++ src/main/java/com/gboihere/overworldoverhaul/mixins/ArmorMaterialsMixin.java
@@ -1,6 +1,5 @@
 package com.gboihere.overworldoverhaul.mixins;
 
-import com.sun.org.apache.bcel.internal.generic.INVOKEINTERFACE;
 import net.minecraft.item.ArmorMaterials;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +10,8 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public class ArmorMaterialsMixin {
 
     @ModifyArg(method = "ArmorMaterials", at = @At(value = "INVOKE", target = "net/minecraft/item/ArmorMaterials")) //No idea how to format this.
-    
+    private int uwu(int starting_value) {
+    return 
+    }
 
 }
