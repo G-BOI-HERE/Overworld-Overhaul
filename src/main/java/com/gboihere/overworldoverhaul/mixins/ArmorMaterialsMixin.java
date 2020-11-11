f8a8ba51e0e05570bfe78d1f6cf87430746e1c69
G-BOI-HERE <G-BOI-HERE@users.noreply.github.com>
2020-11-10 20:05:53 -0500

    ToolMaterials Pre-redirect

--- src/main/java/com/gboihere/overworldoverhaul/mixins/ArmorMaterialsMixin.java
+++ src/main/java/com/gboihere/overworldoverhaul/mixins/ArmorMaterialsMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ArmorMaterials.class)
 
-public class ArmorMaterialsMixin {
+public class ArmorMaterialsMixin{
 
     private final String name;
     int [] protectionAmounts;
@@ -22,21 +22,26 @@ public class ArmorMaterialsMixin {
 
     @Inject(method = "<init>", at = @At(value = "TAIL"))
     private void ArmorMaterials(CallbackInfo ci){
-        if (name.equals("diamond"))
-            protectionAmounts = new int[]{2,6,5,2};
-            durabilityMultiplier = 20;
-            enchantability = 11;
-            toughness = 1.0F;
-        if (name.equals("gold"))
-            protectionAmounts = new int[] {2,6,5,2};
-            durabilityMultiplier = 15;
-            enchantability = 25;
-            toughness = 0.0F;
-        if (name.equals("iron"))
-            protectionAmounts = new int[] {1,5,4,1};
-            durabilityMultiplier = 10;
-            enchantability = 9;
-            toughness = 0.0F;
+        switch (name) {
+            case "diamond":
+                protectionAmounts = new int[]{2, 6, 5, 2};
+                durabilityMultiplier = 20;
+                enchantability = 11;
+                toughness = 1.0F;
+                break;
+            case "gold":
+                protectionAmounts = new int[]{2, 6, 5, 2};
+                durabilityMultiplier = 15;
+                enchantability = 25;
+                toughness = 0.0F;
+                break;
+            case "iron":
+                protectionAmounts = new int[]{1, 5, 4, 1};
+                durabilityMultiplier = 10;
+                enchantability = 9;
+                toughness = 0.0F;
+                break;
+        }
     }
 
 }
--- src/main/java/com/gboihere/overworldoverhaul/mixins/ToolMaterialsMixin.java
+++ src/main/java/com/gboihere/overworldoverhaul/mixins/ToolMaterialsMixin.java
@@ -2,32 +2,9 @@ package com.gboihere.overworldoverhaul.mixins;
 
 import net.minecraft.item.ToolMaterials;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 
 
 @Mixin(ToolMaterials.class)
+public class ToolMaterialsMixin{
 
-public abstract class ToolMaterialsMixin{
-
-    int miningLevel;
-    int itemDurability;
-    float miningSpeed;
-    float attackDamage;
-    int enchantability;
-
-
-    @Inject(method = "<init>", at = @At(value = "TAIL"))
-    private void ToolMaterialsMixin(CallbackInfo ci) {
-
-          if () {
-            miningLevel = 1;
-            itemDurability = 1;
-            miningSpeed = 1;
-            attackDamage = -3;
-            enchantability = 0;
-        }
-    }
 }
