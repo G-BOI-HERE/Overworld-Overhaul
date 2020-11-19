d82e12063c054f2fce4fe469c89097bcb5133c98
G-BOI-HERE <G-BOI-HERE@users.noreply.github.com>
2020-11-18 22:59:09 -0500

    itemsmixin register

--- src/main/java/com/gboihere/overworldoverhaul/mixins/SwordItemsMixin.java
+++ src/main/java/com/gboihere/overworldoverhaul/mixins/SwordItemsMixin.java
@@ -1,39 +1,89 @@
 package com.gboihere.overworldoverhaul.mixins;
 
 
-import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
 import net.minecraft.item.ToolMaterial;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.item.Vanishable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import static net.minecraft.item.ToolMaterials.WOOD;
 
 
 @Mixin(SwordItem.class)
 
-public class SwordItemsMixin {
-
-    @Mutable
-    @Final
-    @Shadow
-    private float attackDamage;
-
-    public SwordItemsMixin(float attackDamage) {
-        this.attackDamage = attackDamage;
+public class SwordItemsMixin extends ToolItem implements Vanishable {
+    public SwordItemsMixin(ToolMaterial material, Settings settings) {
+        super(material, settings);
     }
-
-    @ModifyVariable(method = "<init>", at = @At(value = "HEAD"), ordinal = 3)
-    private static int ItemsMixin(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Item.Settings settings) {
-        System.out.println(toolMaterial);
-        return attackDamage;
-    }
+//
+//    @Mutable
+//    @Final
+//    @Shadow
+//    private float attackDamage;
+//    @Mutable
+//    @Final
+//    @Shadow
+//    private Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+//
+//    public SwordItemsMixin(ToolMaterial material, Settings settings) {
+//        super(material, settings);
+//    }
+//
+//
+//    @Inject(method = "<init>", at = @At(value = "FIELD"))
+//    private void ItemsMixin(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Item.Settings settings, CallbackInfo ci) {
+//        this.attackDamage = (float) attackDamage + toolMaterial.getAttackDamage();
+//        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+//        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", (double) this.attackDamage, EntityAttributeModifier.Operation.ADDITION));
+//        builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Weapon modifier", (double) attackSpeed, EntityAttributeModifier.Operation.ADDITION));
+//        this.attributeModifiers = builder.build();
+//
+//        if (toolMaterial.equals(WOOD)) {
+//            this.attackDamage = 10;
+//            attackSpeed = 0;
+//        }
+//    }
+//
+//    public float getAttackDamage() {
+//        return this.attackDamage;
+//    }
+//
+//    public boolean canMine(BlockState state, World world, BlockPos pos, PlayerEntity miner) {
+//        return !miner.isCreative();
+//    }
+//
+//    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+//        if (state.isOf(Blocks.COBWEB)) {
+//            return 15.0F;
+//        } else {
+//            Material material = state.getMaterial();
+//            return material != Material.PLANT && material != Material.REPLACEABLE_PLANT && material != Material.UNUSED_PLANT && !state.isIn(BlockTags.LEAVES) && material != Material.GOURD ? 1.0F : 1.5F;
+//        }
+//    }
+//
+//    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+//        stack.damage(1, (LivingEntity) attacker, (Consumer<LivingEntity>) ((e) -> {
+//            e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
+//        }));
+//        return true;
+//    }
+//
+//    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+//        if (state.getHardness(world, pos) != 0.0F) {
+//            stack.damage(2, (LivingEntity) miner, (Consumer<LivingEntity>) ((e) -> {
+//                e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
+//            }));
+//        }
+//
+//        return true;
+//    }
+//
+//    public boolean isEffectiveOn(BlockState state) {
+//        return state.isOf(Blocks.COBWEB);
+//    }
+//
+//    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+//        return slot == EquipmentSlot.MAINHAND ? this.attributeModifiers : super.getAttributeModifiers(slot);
+//    }
 }
 
 
---
+++ src/main/java/com/gboihere/overworldoverhaul/mixins/ToolItemsMixin.java
@@ -0,0 +1,234 @@
+package com.gboihere.overworldoverhaul.mixins;
+
+import com.gboihere.overworldoverhaul.item_materials.ModToolMaterials;
+import com.gboihere.overworldoverhaul.items.tools.CustomSwordItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+
+@Mixin(Items.class)
+
+public class ToolItemsMixin {
+
+//    private static Item register(String id, Item item) {
+//        return register(String.valueOf(new Identifier(id)), item);
+//    }
+    @Shadow
+    public static Item register(String id, Item item);
+    /**
+     * @author
+     */
+    @Overwrite
+    private static Item register(String id, Item item) {
+        //Swords
+        if (id.equals("wooden_sword")) {
+            return register(String.valueOf(new Identifier(id)), new CustomSwordItem(ModToolMaterials.WOOD, 10, -0.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+        }
+        return register((new Identifier(id)), item);
+        //            case "stone_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.GOLD, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.GOLD, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.GOLD, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.DIAMOND, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.NETHERITE, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            //Axes
+//            case "wooden_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.WOOD, 3, -3.2F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.STONE, 3, -3.2F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.IRON, 3, -3.2F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.GOLD, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.DIAMOND, 3, -3.1F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.NETHERITE, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            //Pickaxes
+//            case "wooden_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.WOOD, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.STONE, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.IRON, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.GOLD, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.DIAMOND, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.NETHERITE, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            //Shovels
+//            case "wooden_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.WOOD, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.STONE, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.IRON, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.GOLD, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.DIAMOND, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.NETHERITE, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            //Hoes
+//            case "wooden_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.WOOD, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.STONE, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.IRON, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.GOLD, 3, -2.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.DIAMOND, 3, -2.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.NETHERITE, 3, -0.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//        }
+//    @Inject(method = "register(Ljava/lang/String;Lnet/minecraft/item/Item;)Lnet/minecraft/item/Item;",at = @At(value = "FIELD"))
+//    private static void ItemsMixin(String id, Item item, CallbackInfoReturnable<Item> cir){
+//        if (id.equals("wooden_sword")){
+//            System.out.println(cir.getReturnValue());
+//
+//        }
+//
+//    }
+
+
+        //Swords
+//        switch () {
+//            case "wooden_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.GOLD, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.GOLD, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.GOLD, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.GOLD, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.DIAMOND, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_sword":
+//                cir.setReturnValue(new CustomSwordItem(ModToolMaterials.NETHERITE, 0, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            //Axes
+//            case "wooden_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.WOOD, 3, -3.2F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.STONE, 3, -3.2F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.IRON, 3, -3.2F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.GOLD, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.DIAMOND, 3, -3.1F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_axe":
+//                cir.setReturnValue(new CustomAxeItem(ModToolMaterials.NETHERITE, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            //Pickaxes
+//            case "wooden_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.WOOD, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.STONE, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.IRON, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.GOLD, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.DIAMOND, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_pickaxe":
+//                cir.setReturnValue(new CustomPickaxeItem(ModToolMaterials.NETHERITE, 3, -2.8F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            //Shovels
+//            case "wooden_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.WOOD, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.STONE, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.IRON, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.GOLD, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.DIAMOND, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_shovel":
+//                cir.setReturnValue(new CustomShovelItem(ModToolMaterials.NETHERITE, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            //Hoes
+//            case "wooden_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.WOOD, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "stone_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.STONE, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "iron_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.IRON, 3, -3.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "golden_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.GOLD, 3, -2.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "diamond_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.DIAMOND, 3, -2.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//            case "netherite_hoe":
+//                cir.setReturnValue(new CustomHoeItem(ModToolMaterials.NETHERITE, 3, -0.0F, (new Item.Settings()).group(ItemGroup.COMBAT)));
+//                break;
+//        }
+    }
+}
+
